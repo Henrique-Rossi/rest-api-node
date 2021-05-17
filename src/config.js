3b607b9fe7132c31dc6813ae980615c94f61d0f5
Henrique-Rossi <62766483+Henrique-Rossi@users.noreply.github.com>
2021-05-16 22:21:29 -0300

    first commit

---
+++ src/config.js
@@ -0,0 +1,8 @@
+global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, Bem vindo à minha api!';
+
+module.exports = {
+    connectionString: 'mongodb+srv://cluster0.c5wgi.mongodb.net/apirest',
+    sendgridKey: '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',
+   
+}
